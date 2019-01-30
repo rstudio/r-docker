9ed0786b55318422252fd3e7d766f17b17f59d24
Greg Lin <glin@rstudio.com>
2019-01-30 14:42:17 -0600

    Test installing a package with compilation

--- test/test.sh
+++ test/test.sh
@@ -2,4 +2,9 @@
 set -ex
 
 R --version
+
+# Install a package without compilation
 Rscript -e 'install.packages("R6", repos = "https://cloud.r-project.org"); library(R6)'
+
+# Install a package with compilation
+Rscript -e 'install.packages("BASIX", repos = "https://cloud.r-project.org"); library(BASIX)'
