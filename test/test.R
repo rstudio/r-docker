a0b7442fb442c2f8bdc8b85b39d77d510ae2b55b
Greg Lin <glin@rstudio.com>
2019-03-18 10:26:43 -0500

    Add missing time zone database on xenial/bionic

--- test/test.R
+++ test/test.R
@@ -8,3 +8,9 @@ library(R6)
 # Install a package with compilation
 install.packages("BASIX")
 library(BASIX)
+
+# Check that the time zone database is present
+# https://stat.ethz.ch/R-manual/R-devel/library/base/html/timezones.html
+if (length(OlsonNames()) == 0) {
+  stop("Time zone database not found")
+}
