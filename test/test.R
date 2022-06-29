8a041c8017ed4dc708272732b929d6308205e4e2
Greg Lin <glin@rstudio.com>
2022-06-29 12:08:46 -0500

    Fix tests that depend on removed CRAN package
    BASIX tested that R could install packages with compiled code.
    This is already tested via the local test package, so there's no
    need for it anymore.

--- test/test.R
+++ test/test.R
@@ -1,13 +1,9 @@
 # HTTP mirror to support R 3.1
 options(repos = c("https://cloud.r-project.org", "http://cloud.r-project.org"))
 
-# Install a package without compilation
+# Check that packages can be installed
 install.packages("R6")
 library(R6)
-
-# Install a package with compilation
-install.packages("BASIX")
-library(BASIX)
 
 # Check that the time zone database is present
 # https://stat.ethz.ch/R-manual/R-devel/library/base/html/timezones.html
