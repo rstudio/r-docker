2bbb8d24bc2fa30e678fcfc41107c8f8a04e71c8
Greg Lin <glin@posit.co>
2025-01-06 13:30:28 -0600

    Fix tests for R-devel, switch to public R API functions

--- test/testpkg/src/subtract.cpp
+++ test/testpkg/src/subtract.cpp
@@ -3,8 +3,8 @@
 
 extern "C" SEXP subtract(SEXP a, SEXP b)
 {
-  SEXP result = PROTECT(allocVector(REALSXP, 1));
-  REAL(result)[0] = asReal(a) - asReal(b);
+  SEXP result = PROTECT(Rf_allocVector(REALSXP, 1));
+  REAL(result)[0] = Rf_asReal(a) - Rf_asReal(b);
   UNPROTECT(1);
   return result;
 }
